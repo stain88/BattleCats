8e814381e633b64c8d26aea0857044273da31c54
stain88 <marcbaghdadi@gmail.com>
2015-10-08 22:27:58 +0100

    Added canvas, enter_frame function

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,14 @@
+//create Enter_Frame function handler
+var animate = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) {window.setTimeout(callback,1000/60)};
+
+//
+
 $(function() {
-  
+  animate(step);
 })
+
+
+function step (timestamp) {
+  console.log(timestamp);
+  //window.requestAnimationFrame(animate);
+}
