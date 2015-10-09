5faeda971418f2deb7ae89e3922c2408ce793906
stain88 <marcbaghdadi@gmail.com>
2015-10-09 17:46:12 +0100

    Added transform library, grid scalings

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,9 @@
 var MAX_BOXES = 6;
-var selectedCells = [];
+var selectedPlayerCells = [];
+var playerSelectionBoard;
+var playerAttackBoard;
+var computerSelectionBoard;
+var computerAttackBoard;
 
 $(function() {
   setupBoard();
@@ -12,7 +16,9 @@ function setupBoard() {
     for (var j=0;j<MAX_BOXES;j++) {
       $('tbody').find('tr:last').append($('<td>',{'data-num':"cell"+i+j}));
     }
-  }  
+  }
+  playerSelectionBoard = $('#playerBoard');
+  playerAttackBoard = playerSelectionBoard.clone();
 }
 
 function addListeners() {
@@ -25,9 +31,9 @@ function addListeners() {
     isMouseDown = true;
     $(this).toggleClass("placedShip")
     if ($(this).attr('class')==="placedShip") {
-      selectedCells.push($(event.target).attr("data-num"));
+      selectedPlayerCells.push($(event.target).attr("data-num"));
     } else {
-      selectedCells.splice(selectedCells.indexOf($(event.target).attr("data-num")),1);
+      selectedPlayerCells.splice(selectedPlayerCells.indexOf($(event.target).attr("data-num")),1);
     }
   })
   .on("mouseover", function() {
@@ -35,19 +41,20 @@ function addListeners() {
       $(this).toggleClass("placedShip")
       console.log($(event.target).attr("data-num").substr(4,2).split("").map(function(x){return parseInt(x,10)}));
       if ($(this).attr('class')==="placedShip") {
-        selectedCells.push($(event.target).attr("data-num"));
+        selectedPlayerCells.push($(event.target).attr("data-num"));
       } else {
-        selectedCells.splice(selectedCells.indexOf($(event.target).attr("data-num")),1);
+        selectedPlayerCells.splice(selectedPlayerCells.indexOf($(event.target).attr("data-num")),1);
       }
     }
   })
   $(document).on("mouseup", function() {
     isMouseDown = false;
   })
-  $('#startGame').on("click",toggleSelector)
+  $('#startGame').on("click",startGame)
 }
 
-function toggleSelector() {
+function startGame() {
   $('td').off();
-  // $('#playerBoard').
+  playerSelectionBoard.animate({transform: 'scale(0.5)'}).animate({transform: 'translateX(-400px) translateY(-100px) scale(0.5)'});
+  playerAttackBoard.show().fadeIn(1000);
 }
