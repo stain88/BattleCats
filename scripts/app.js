e790614aaf736180d1fc6802caf9b20168bdea28
stain88 <marcbaghdadi@gmail.com>
2015-10-09 14:21:23 +0100

    Added start button, toggle ship placement

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,41 @@
-var MAX_BOXES = 5;
+var MAX_BOXES = 6;
 
 $(function() {
+  setupBoard();
+  addListeners();
+})
+
+function setupBoard() {
   for (var i=0;i<MAX_BOXES;i++) {
     $('tbody').append($(document.createElement('tr')));
     for (var j=0;j<MAX_BOXES;j++) {
-      $('tbody').find('tr:last').append($(document.createElement('td')));
+      $('tbody').find('tr:last').append($('<td>',{'data-num':"cell"+i+j}));
     }
-  }
-})
+  }  
+}
+
+function addListeners() {
+  var isMouseDown = false;
+  var startingCell;
+  var selectedCells=[];
+  $('td').on("click", function() {
+    console.log(event.target);
+  })
+  .on("mousedown", function() {
+    isMouseDown = true;
+    $(this).toggleClass("placedShip")
+  })
+  .on("mouseover", function() {
+    if (isMouseDown) {
+      $(this).toggleClass("placedShip")
+    }
+  })
+  $(document).on("mouseup", function() {
+    isMouseDown = false;
+  })
+  $('#startGame').on("click",toggleSelector)
+}
+
+function toggleSelector() {
+  $('td').off();
+}
